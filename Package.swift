cf9d4412b0b56a18a7046ece239abe4f2ae7a794
donghyuk <donghyuk@rainist.com>
2021-01-14 22:44:46 +0900

    Update package type library to executable

--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
     name: "Game",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Game",
-            targets: ["Game"]),
+        .executable(
+            name: "Game", 
+            targets: ["Game"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
